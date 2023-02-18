fa415c275cfa07d08a6ba24a750da94455b4824c
ANKUSH PRAJAPATI <ankushprajapati77@gmail.com>
2023-02-18 11:20:03 +0530

    https req

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import MovieCard from './MovieCard';
 
 import './App.css';
 import SearchIcon from './search.svg';
-const API_URL = 'http://www.omdbapi.com?apikey=660b228f';
+const API_URL = 'https://www.omdbapi.com?apikey=660b228f';
 
 // const movie1 =
 //     {
